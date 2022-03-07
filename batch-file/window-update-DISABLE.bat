e9eeaad42fdd4935b556d40c5a49fef67e13c5d2
idhirandar <rk363740@gmail.com>
2022-03-07 13:21:31 +0530

    add window update manager script

---
+++ batch-file/window-update-DISABLE.bat
@@ -0,0 +1,11 @@
+@Echo Stopping the Windows Update Service ...
+@echo off
+Net stop wuauserv
+@echo ***********************************
+@Echo Disabling the Windows Update Service ...
+@echo off
+sc config "wuauserv" start=disabled
+@echo ***********************************
+@echo Finished ....
+pause
+
